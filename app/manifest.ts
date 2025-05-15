26e1d2f5c1e560aad0b94bdf671178f2bab00b8f
LongLive16887 <mustafoyevnew@gmail.com>
2025-05-15 22:14:44 +0500

    build

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,22 +11,10 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0ea5e9",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icon",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
-      },
-      {
-        src: "/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/icons/maskable-icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
       },
     ],
   }
